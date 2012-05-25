fd68066192daf3db22778522220ce846599f4f65
Sander Temme <sander@temme.net>
2012-05-24 20:30:55 -1000

    Base template for new applications

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ rails-templates
 ===============
 
 My own useful templates for Rails apps.
+
+base.rb -- Rails application with RSpec and Cucumber, checked into a new Git repository.
---
+++ base.rb
@@ -0,0 +1,53 @@
+# Basic template to set up new apps with the stuff I always do anyway.
+# Templates (canonically?) documented at:
+# http://guides.rubyonrails.org/generators.html#application-templates
+
+git :init
+
+# file ".gitignore", <<-END
+# .bundle
+# db/*.sqlite3
+# log/*.log
+# tmp/
+# .sass-cache/
+# .idea
+# public/assets
+# END
+
+gem_group :development, :test do
+  gem 'rspec-rails'
+#  gem 'sqlite3'
+end
+
+gem_group :test do
+#   gem 'database_cleaner'
+#   gem 'factory_girl'
+#   gem 'spork'
+#   gem 'launchy'
+  gem 'cucumber-rails'
+end
+
+# gem_group :staging, :production do
+#   gem 'pg'
+# end
+
+insert_into_file 'config/application.rb', :after => "Rails::Application\n" do <<-END
+
+    # Prevent the scaffold generator from creating RSpec files for views
+    # and helpers.
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.request_specs false
+    end
+
+END
+end
+
+run "bundle install"
+
+generate "rspec:install"
+generate "cucumber:install"
+
+git :add => "."
+git :commit => "-m 'initial commit'"
